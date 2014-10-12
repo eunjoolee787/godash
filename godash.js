dca41a0554817144d1abc958be100dd0f53ad754
Eun Joo <eunjoolee787@gmail.com>
2014-10-11 19:25:00 -1000

    part 1 done

--- godash.js
+++ godash.js
@@ -2,9 +2,22 @@
 // put this code in here!
  
 module.exports = {
-  // compact: function(){},
+  compact: function(array){
+ 	var results = [];
+
+  	for(var i = 0; i < array.length; i++){
+  	  if(array[i]){
+		
+	    results.push(array[i])
+  	 }
+
+  	//console.log(array[i]);
+    }
+  	 return results;
+  }
   // difference: function(){},
   //etc...
-  test: function(){console.log("test! rename this file to your project name")}
+  //test: function(){console.log("test! rename this file to your project name")}
 };
  
+
